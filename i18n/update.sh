1eb08b156fc21af75697ca595cf0f295b2ce97bf
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2011-03-03 15:00:54 +0000

    Renombrar los nombres de archivos de VenezuelanEidAuthPlugin-plone-* a plone-VenezuelanEidAuthPlugin-* en este bash script

--- i18n/update.sh
+++ i18n/update.sh
@@ -1,6 +1,21 @@
-#i18ndude rebuild-pot --pot collective.amberjack.portlet.pot --create collective.amberjack.portlet --merge VenezuelanEidAuthPlugin-manual.pot ../
-i18ndude rebuild-pot --pot VenezuelanEidAuthPlugin.pot --create venezuelaneidauthplugin ../
-i18ndude sync --pot VenezuelanEidAuthPlugin.pot VenezuelanEidAuthPlugin-*.po
+i18ndude rebuild-pot --pot ./VenezuelanEidAuthPlugin.pot --merge ./manual.pot --create venezuelaneidauthplugin ../
+i18ndude sync --pot ./VenezuelanEidAuthPlugin.pot ./VenezuelanEidAuthPlugin-*.po
 
-i18ndude rebuild-pot --pot VenezuelanEidAuthPlugin-plone.pot --create plone ../
-i18ndude sync --pot VenezuelanEidAuthPlugin-plone.pot VenezuelanEidAuthPlugin-plone-*.po
+i18ndude rebuild-pot --pot plone-VenezuelanEidAuthPlugin.pot --merge ./plone-manual.pot --create plone ../skins/
+i18ndude sync --pot plone-VenezuelanEidAuthPlugin.pot plone-VenezuelanEidAuthPlugin-*.po
+
+ERRORS=`find ../ -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+WARNINGS=`find ../ -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+FATAL=`find ../ -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or" 
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir" 
+
+rm ./rebuild_i18n.log
+touch ./rebuild_i18n.log
+
+find ../ -name "*pt" | xargs i18ndude find-untranslated > ./rebuild_i18n.log
